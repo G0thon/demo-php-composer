6afd9af72827fa1f3d69bc5e19215790ee88dc25
Gothon <gauthier.cosson@protonmail.com>
2017-10-10 19:42:06 +0200

    Feat. Add style

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
   <head>
     <title>CleverCloud composer</title>
     <link rel="stylesheet" href="/public/css/main.css" type="text/css" media="all">
+    <style type="text/css">
+      background-color: white;
+      color: black;
+    </style>
   </head>
   <body>
     <?php require __DIR__ . '/vendor/autoload.php'; ?>
